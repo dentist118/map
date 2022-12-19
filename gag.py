27186bc2482690d68c31ec202d342d691905f32b
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-20 00:50:16 +0200

    Update gag.py

--- gag.py
+++ gag.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title("DR. MAHMOUD SITE.")
 
 image = Image.open('https://cdn-images-1.medium.com/max/1024/1*u9U3YjxT9c9A1FIaDMonHw.png')
-st.image(image,use_column_width=False)
+st.image(image,use_column_width=True)
 
 n_words =st.number_input('Type the number of words you want to generate')
 seed_text =st.text_input('Type the number of words you want to generate after')
