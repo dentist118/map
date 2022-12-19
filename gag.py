82b4c01e7379990a0e7af25e5abfcd733c45bebf
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-20 01:19:24 +0200

    Update gag.py

--- gag.py
+++ gag.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from PIL import Image
- 
 st.title("DR. MAHMOUD SITE.")
 
-image = Image.open('https://cdn-images-1.medium.com/max/1024/1*u9U3YjxT9c9A1FIaDMonHw.png')
+image = Image.open("Users/md/Desktop/mody.gif")
 st.image(image,use_column_width=True)
+
 
 n_words =st.number_input('Type the number of words you want to generate')
 seed_text =st.text_input('Type the number of words you want to generate after')
@@ -14,3 +14,4 @@ if n_words or seed_text:
     st.warning("generate_text")
 else:
     st.warning("Please input a word and a number")
+
