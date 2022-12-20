02859b9062dd2d9e544bfdd368ac739dbb4a5403
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-20 02:09:56 +0200

    Update gag.py

--- gag.py
+++ gag.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 st.title("DR. MAHMOUD SITE.")
 
-image = Image.open("C:/Users/md/Videos/ملف مهم/medo1.jpg")
+image = Image.open("C:/medo1.jpg")
 st.image(image,use_column_width=True)
 
 
@@ -14,6 +14,4 @@ if n_words or seed_text:
     st.warning("generate_text")
 else:
     st.warning("Please input a word and a number")
-
-
 
