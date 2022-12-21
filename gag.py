6469164020a8a9bc58d9541f87dc4f91e095b0d7
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-21 19:24:55 +0200

    Update gag.py

--- gag.py
+++ gag.py
@@ -5,10 +5,19 @@ from PIL import Image ,ImagePath
 st.title("DR. MAHMOUD SITE.")
 #if st.checkbox('checkbox'):
    # st.image('medo1.png')
-url =("https://bgremoval.streamlit.app/~/+/media/e41ef1cefe668882972d74a560811abe2134ebe7cdf41d69e50f5ae8.png")
-#image = Image.open("url")
-st.image([url])
+url = "https://bgremoval.streamlit.app/~/+/media/e41ef1cefe668882972d74a560811abe2134ebe7cdf41d69e50f5ae8.png"
+caption = "ahoy!"
 
+        
+st.image([url] * 1, caption=[caption] * 1)
+st.image(
+            imgs,
+            caption=["some caption"] * 1,
+            width=200,
+            use_column_width=True,
+            clamp=True,
+            
+        )
 
 n_words =st.number_input('Type the number of words you want to generate')
 seed_text =st.text_input('Type the number of words you want to generate after')
