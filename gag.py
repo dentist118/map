5c298ae522be1eba4aa4b6d3cda4d447faad4169
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-21 19:11:17 +0200

    Update gag.py

--- gag.py
+++ gag.py
@@ -6,7 +6,7 @@ st.title("DR. MAHMOUD SITE.")
 #if st.checkbox('checkbox'):
    # st.image('medo1.png')
 
-image = Image.open('https://bgremoval.streamlit.app/~/+/media/e41ef1cefe668882972d74a560811abe2134ebe7cdf41d69e50f5ae8.png')
+image = Image.open("https://bgremoval.streamlit.app/~/+/media/e41ef1cefe668882972d74a560811abe2134ebe7cdf41d69e50f5ae8.png")
 st.image(image)
 
 
