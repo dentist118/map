4dff3f899a5eedeef1838eb2d7a4bc04eea86429
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-21 21:17:19 +0200

    Update gag.py

--- gag.py
+++ gag.py
@@ -10,7 +10,7 @@ url = "https://raw.githubusercontent.com/danielgatis/rembg/master/examples/anima
 st.image(
             [url],
             caption=["congrats"] ,
-          image.resize(300, 200),
+          image.resize(300, 200)
            )
 
 #caption = "hello!"
